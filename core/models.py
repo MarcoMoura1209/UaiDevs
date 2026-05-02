7b4b31f1d00672665ba31e300368c89dc9fead1a
Marco Moura <marcomoura27052009@gmail.com>
2026-05-02 13:11:41 -0300

    Adiciona modelo Cliente com campos de contato e atualiza a configuração do Django para incluir o campo de telefone

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    mensagem = models.TextField(blank=False, null=False)
+    telefone = PhoneNumberField(region="BR", blank=False, null=False)
+    empresa = models.CharField(max_length=30)
+
+    class Meta: 
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+
+    def __str__(self):
+        return self.nome

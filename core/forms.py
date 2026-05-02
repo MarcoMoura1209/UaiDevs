79f34461a8a157b2f4f3e8d30a3f64618072f411
Marco Moura <marcomoura27052009@gmail.com>
2026-05-02 13:25:34 -0300

    Adiciona formulário para o modelo Cliente com validação de mensagem

---
+++ core/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Cliente
+
+
+class Form(forms.ModelForm):
+    class Meta:
+        model = Cliente
+
+        fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'campo-nome campo',
+                'placeholder': 'Seu nome'}),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'campo-email campo',
+                'placeholder': 'exemplo@email.com'}),
+
+            'mensagem': forms.Textarea(attrs={
+                'class': 'campo-mensagem campo',
+                'placeholder': 'Deixe aqui sua mensagem...',
+                'maxlength': '1500'}),
+
+            'telefone': forms.TextInput(attrs={
+                'class': 'campo-telefone campo',
+                'placeholder': '(00)00000-0000',}),
+
+            'empresa': forms.TextInput(attrs={
+                'class': 'campo-empresa campo',
+                'placeholder': 'Nome da empresa'})
+        }
+
+    def clean_mensagem(self):
+        mensagem = self.cleaned_data.get('mensagem')
+        if len(mensagem) > 1500:
+            raise forms.ValidationError(
+                'A mensagem deve ter no máximo 1500 caracteres.')
+        return mensagem

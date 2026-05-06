9e12cf5ae997036ebcd80aa585462498267dccb0
Marco Moura <marcomoura27052009@gmail.com>
2026-05-06 14:12:04 -0300

    Ajusta o campo 'mensagem' do modelo 'Cliente' para ter um limite máximo de 1500 caracteres

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Cliente(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
-    mensagem = models.TextField(blank=False, null=False)
+    mensagem = models.TextField(max_length=1500, blank=False, null=False)
     telefone = PhoneNumberField(region="BR", blank=False, null=False)
     empresa = models.CharField(max_length=30)
 

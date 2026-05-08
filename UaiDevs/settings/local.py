a56586e14b9def5de04194f832cb8f0eb460425c
Marco Moura <marcomoura27052009@gmail.com>
2026-05-08 16:42:56 -0300

    Adiciona configuração de logging para arquivo e ajusta manipuladores para Django e core

--- UaiDevs/settings/local.py
+++ UaiDevs/settings/local.py
@@ -1,5 +1,6 @@
 from .base import *
 import copy
+import os
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
@@ -7,8 +8,21 @@ RATELIMIT_ENABLE = False
 
 LOGGING = copy.deepcopy(LOGGING)
 
+LOGGING['handlers']['file'] = {
+    'level': 'DEBUG',
+    'class': 'logging.handlers.RotatingFileHandler',
+    'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+    'maxBytes': 1024 * 1024 * 5,  # 5MB
+    'backupCount': 3,
+    'formatter': 'simple',
+}
+
+LOGGING['loggers']['django']['handlers'] = ['console', 'file']
 LOGGING['loggers']['django']['level'] = 'DEBUG'
+
+LOGGING['loggers']['core']['handlers'] = ['console', 'file']
 LOGGING['loggers']['core']['level'] = 'DEBUG'
+
 LOGGING['loggers']['django.db.backends'] = {
     'level': 'DEBUG',
     'handlers': ['console'],

d8f24860a082fcf0d9acdf90bfaf43a9e3725bb2
Marco Moura <marcomoura27052009@gmail.com>
2026-05-09 11:09:07 -0300

    Adiciona testes para o modelo Cliente e configurações do pytest

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Cliente
 
-# Create your tests here.
+
+class ClientModelTest(TestCase):
+
+    def setUp(self):
+        self.cliente = Cliente.objects.create(
+            nome = 'Homem Teste',
+            email = 'homemteste@gmail.com',
+            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
+            telefone = '+5538999999999',
+            empresa = 'Empresa Teste',
+        )
+        return super().setUp()
+
+    def test_cliente_criado_com_sucesso(self):
+        self.assertEqual(Cliente.objects.count(), 1)

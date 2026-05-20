1631c05c79b3e78e41b1b7df63bccdc386c72f9a
Marco Moura <marcomoura27052009@gmail.com>
2026-05-20 14:08:07 -0300

    Refactor spacing in Cliente model test cases for consistency

--- core/tests/test_model_core.py
+++ core/tests/test_model_core.py
@@ -6,11 +6,11 @@ class ClientModelTest(TestCase):
 
     def setUp(self):
         self.cliente = Cliente.objects.create(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '38999999999',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='38999999999',
+            empresa='Empresa Teste',
         )
         return super().setUp()
 
@@ -28,11 +28,11 @@ class ClientModelTest(TestCase):
         '''Nome nao pode ultrapassar mais de 30 caracteres'''
 
         cliente_invalido = Cliente(
-            nome = 'x' * 31,
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '38999999999',
-            empresa = 'Empresa Teste',
+            nome='x' * 31,
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='38999999999',
+            empresa='Empresa Teste',
         )
         with self.assertRaises(Exception):
             cliente_invalido.full_clean()
@@ -42,11 +42,11 @@ class ClientModelTest(TestCase):
         '''Nome da empresa nao pode ultrapassar mais de 30 caracteres'''
 
         cliente_invalido = Cliente(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '38999999999',
-            empresa = 'x' * 31,
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='38999999999',
+            empresa='x' * 31,
         )
         with self.assertRaises(Exception):
             cliente_invalido.full_clean()
@@ -56,11 +56,11 @@ class ClientModelTest(TestCase):
         '''Mensagem nao pode ultrapassar mais de 1500 caracteres'''
 
         cliente_invalido = Cliente(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'x' * 1501,
-            telefone = '38999999999',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='x' * 1501,
+            telefone='38999999999',
+            empresa='Empresa Teste',
         )
         with self.assertRaises(Exception):
             cliente_invalido.full_clean()
@@ -70,11 +70,11 @@ class ClientModelTest(TestCase):
         '''Telefone com formato invalido deve ser rejeitado'''
 
         cliente_invalido = Cliente(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '123',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='123',
+            empresa='Empresa Teste',
         )
         with self.assertRaises(Exception):
             cliente_invalido.full_clean()
@@ -82,11 +82,11 @@ class ClientModelTest(TestCase):
 
     def test_telefone_valido_com_formato_correto(self):
         cliente_valido = Cliente(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '11999999999',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='11999999999',
+            empresa='Empresa Teste',
         )
 
         cliente_valido.full_clean()
@@ -94,11 +94,11 @@ class ClientModelTest(TestCase):
 
     def test_email_invalido_sem_arroba (self):
         cliente_invalido = Cliente(
-            nome = 'Homem Teste',
-            email = 'email-sem-arroba-gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '11999999999',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='email-sem-arroba-gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='11999999999',
+            empresa='Empresa Teste',
         )
         with self.assertRaises(Exception):
             cliente_invalido.full_clean()
@@ -106,11 +106,11 @@ class ClientModelTest(TestCase):
 
     def test_email_com_formato_valido(self):
         cliente_valido = Cliente(
-            nome = 'Homem Teste',
-            email = 'homemteste@gmail.com',
-            mensagem = 'Ola, gostaria de poder estar conversando com voces!',
-            telefone = '11999999999',
-            empresa = 'Empresa Teste',
+            nome='Homem Teste',
+            email='homemteste@gmail.com',
+            mensagem='Ola, gostaria de poder estar conversando com voces!',
+            telefone='11999999999',
+            empresa='Empresa Teste',
         )
         cliente_valido.full_clean()
         cliente_valido.clean()

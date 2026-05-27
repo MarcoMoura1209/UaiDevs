5e75693606ab2fd3631e0a37e3b02338b77f34ad
Marco Moura <marcomoura27052009@gmail.com>
2026-05-27 15:14:36 -0300

    adicao e configuracao de sitemap.xml e robots.txt

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('sitemap.xml', views.sitemap, name="sitemap"),
+    path('robots.txt', views.robots, name="robots"),
 ]

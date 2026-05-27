d76625f33231ee771e86c28d8c531bc7ba28abda
Marco Moura <marcomoura27052009@gmail.com>
2026-05-27 15:15:15 -0300

    Autalizacao das viwes com a PEP8

--- core/views.py
+++ core/views.py
@@ -5,10 +5,10 @@ from django.views.decorators.http import require_http_methods
 from .forms import Form
 from django_ratelimit.decorators import ratelimit
 from honeypot.decorators import check_honeypot
- 
- 
+
+
 logger = logging.getLogger('core')
- 
+
 @check_honeypot
 @ratelimit(key='ip', rate='10/h', method='POST', block=True)
 def home(request):
@@ -19,19 +19,19 @@ def home(request):
             return redirect('core:home')
     else:
         form = Form()
- 
+
     return render(request, 'core/pages/home.html', context={
         'form': form,
     })
- 
- 
+
+
 @require_http_methods(["GET"])
 def sitemap(request):
     """Gera e serve o sitemap.xml dinamicamente"""
     sitemap_content = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
- 
+
     <url>
         <loc>https://uaidevs.com.br/</loc>
         <lastmod>2026-05-27</lastmod>
@@ -42,24 +42,23 @@ def sitemap(request):
             hreflang="pt-BR"
             href="https://uaidevs.com.br/"/>
     </url>
- 
+
 </urlset>'''
-    
+
     return HttpResponse(sitemap_content, content_type='application/xml')
- 
- 
+
+
 @require_http_methods(["GET"])
 def robots(request):
     """Gera e serve o robots.txt dinamicamente"""
     robots_content = '''User-agent: *
 Allow: /
- 
+
 Disallow: /admin/
 Disallow: /static/
 Disallow: /media/
  
 Sitemap: https://uaidevs.com.br/sitemap.xml
 '''
-    
+
     return HttpResponse(robots_content, content_type='text/plain')
- 

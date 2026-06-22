9c3e0f5c2f1d1131f1bb3ddbe469fb6ca76a4546
Marco Moura <marcomoura27052009@gmail.com>
2026-06-22 20:01:29 -0300

    Refactor DJANGO_SETTINGS_MODULE environment variable setup for clarity

--- manage.py
+++ manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UaiDevs.settings.local')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        os.getenv(
+            'DJANGO_SETTINGS_MODULE',
+            'UaiDevs.settings.local'
+        )
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
